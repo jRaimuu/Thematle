5fba5a5f924d6fc8aa143f76cef66c4a637eae4b
jRaimuu <liamsarj4@gmail.com>
2023-10-26 22:22:25 -0600

    Added Json data

--- Thematle.js
+++ Thematle.js
@@ -9,101 +9,22 @@
  * frame and having their values updated
  */
 
-const themePack = [
-    {
-        topic: "Medical",
-        words: [
-            { word: "doctor", weight: 0.02 },
-            { word: "nurse", weight: 0.04 },
-            { word: "hospital", weight: 0.06 },
-            { word: "medicine", weight: 0.08 },
-            { word: "patient", weight: 0.1 },
-            { word: "surgery", weight: 0.12 },
-            { word: "diagnosis", weight: 0.14 },
-            { word: "pharmacy", weight: 0.16 },
-            { word: "stethoscope", weight: 0.18 },
-            { word: "ambulance", weight: 0.2 },
-            { word: "injection", weight: 0.22 },
-            { word: "x-ray", weight: 0.24 },
-            { word: "scrubs", weight: 0.26 },
-            { word: "patient", weight: 0.28 },
-            { word: "couch", weight: 0.3 },
-            { word: "lamp", weight: 0.32 },
-            { word: "book", weight: 0.34 },
-            { word: "television", weight: 0.36 },
-            { word: "coffee", weight: 0.38 },
-            { word: "shoes", weight: 0.4 },
-            { word: "pillow", weight: 0.45 },
-            { word: "keyboard", weight: 0.5 },
-            { word: "chair", weight: 0.55 },
-            { word: "monitor", weight: 0.6 },
-            { word: "bottle", weight: 0.65 },
-            { word: "table", weight: 0.7 },
-            { word: "clock", weight: 0.75 },
-            { word: "phone", weight: 0.8 },
-            { word: "laptop", weight: 0.85 },
-            { word: "window", weight: 0.9 },
-            { word: "door", weight: 0.95 },
-            { word: "mirror", weight: 1.0 },
-        ],
-    },
-    {
-        name: "Nature",
-        words: [
-            { word: "tree", weight: 0.02 },
-            { word: "river", weight: 0.04 },
-            { word: "mountain", weight: 0.06 },
-            { word: "wildlife", weight: 0.08 },
-            { word: "forest", weight: 0.1 },
-            { word: "bird", weight: 0.12 },
-            { word: "flower", weight: 0.14 },
-            { word: "lake", weight: 0.16 },
-            { word: "sunset", weight: 0.18 },
-            { word: "camping", weight: 0.2 },
-            { word: "hiking", weight: 0.22 },
-            { word: "wind", weight: 0.24 }, 
-            { word: "rock", weight: 0.26 },
-            { word: "ocean", weight: 0.28 },
-            { word: "sky", weight: 0.3 },
-            { word: "fall", weight: 0.32 }, 
-            { word: "desert", weight: 0.34 },
-            { word: "flower", weight: 0.36 },
-            { word: "flood", weight: 0.38 }, 
-            { word: "lake", weight: 0.4 },
-            { word: "produce", weight: 0.42 }, 
-            { word: "star", weight: 0.44 },
-            { word: "snap", weight: 0.46 }, 
-            { word: "sunrise", weight: 0.48 },
-            { word: "moss", weight: 0.5 }, 
-            { word: "space", weight: 0.52 }, 
-            { word: "beach", weight: 0.54 },
-            { word: "root", weight: 0.56 }, 
-            { word: "oil", weight: 0.58 },
-            { word: "tear", weight: 0.6 }, 
-            { word: "paper", weight: 0.62 }, 
-            { word: "stove", weight: 0.64 },
-            { word: "shot", weight: 0.66 }, 
-            { word: "shower", weight: 0.68 },
-            { word: "tire", weight: 0.7 }, 
-            { word: "cloth", weight: 0.72 }, 
-            { word: "photo", weight: 0.74 },
-            { word: "tear", weight: 0.76 }, 
-            { word: "bass", weight: 0.78 }, 
-            { word: "car", weight: 0.8 }, 
-            { word: "pen", weight: 0.82 }, 
-            { word: "radio", weight: 0.84 },
-            { word: "wire", weight: 0.86 },
-            { word: "tower", weight: 0.88 },
-            { word: "can", weight: 0.9 }, 
-            { word: "lead", weight: 0.92 }, 
-            { word: "row", weight: 0.94 }, 
-            { word: "stool", weight: 0.96 }, 
-            { word: "sewer", weight: 0.98 },
-            { word: "bass", weight: 1.0 },
-        ],
-    },
-];
+const jsonURL = 'https';
 
+fetch(jsonURL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the JSON response
+  })
+  .then(data => {
+    // You can now work with the JSON data
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching JSON:', error);
+  });
 
 
 //Object representing the orange team
@@ -152,9 +73,9 @@ const card = {
 //This will store the avatar icons that will update the icons displayed next to the score
 //each time a players guesses a card
 const nextAvatar = [
-    './assets/image1.png',
-    './assets/image2.png',
-    './assets/image3.png',
+    // './assets/image1.png',
+    // './assets/image2.png',
+    // './assets/image3.png',
 ];
 
 /**
@@ -201,8 +122,8 @@ function revealClue() {
 
 
 //When the decipherer inputs and submits clue
-input.addEventListener('click', revealClue());
+// input.addEventListener('click', revealClue());
 
 //Updates the card graphic
-card - i.addEventListener('click', updateCard());
+// card - i.addEventListener('click', updateCard());
 
